33b4ddd4fe2a0c0ea21eab0a4494a4def766705a
bennyty <bennytytyty@gmail.com>
2016-12-06 14:01:25 -0500

    Temp

--- empac-server/app.js
+++ empac-server/app.js
@@ -1,53 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const restify = require('express-restify-mongoose')
+const app = express()
+const router = express.Router()
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+app.use(bodyParser.json())
+app.use(methodOverride())
 
-var app = express();
+mongoose.connect('mongodb://localhost/empacplus')
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+restify.serve(router, mongoose.model('Events', new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  date: { type: Date }
+})))
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(router)
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// Use native Node promises
-mongoose.Promise = global.Promise;
-// connect to MongoDB
-mongoose.connect('mongodb://localhost/todo-api')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
-module.exports = app;
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000')
+})
