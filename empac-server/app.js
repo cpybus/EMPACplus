da1d77e172d813807f203b47549acb052ce0aafe
bennyty <bennytytyty@gmail.com>
2016-12-10 17:21:15 -0500

    API Working!

--- empac-server/app.js
+++ empac-server/app.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const restify = require('express-restify-mongoose')
 const app = express()
 const router = express.Router()
 
 app.use(bodyParser.json())
-app.use(methodOverride())
+// app.use(methodOverride())
 
 mongoose.connect('mongodb://localhost/empacplus')
+
+restify.serve(router, mongoose.model('Clients', new mongoose.Schema({
+  name: { type: String },
+  event: { type: mongoose.Schema.Types.ObjectId }
+})))
 
 restify.serve(router, mongoose.model('Events', new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  date: { type: Date }
+  date: { type: Date },
+  clients: [{ type: mongoose.Schema.Types.ObjectId }]
 })))
 
 app.use(router)
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log('Express server listening on port 3000')
 })
